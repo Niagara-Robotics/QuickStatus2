ff8f26ea577e14e6cacf5a335a787bd436dadc25
Ian <72885443+40F4@users.noreply.github.com>
2026-03-10 14:04:41 -0400

    Properly show windows icon

--- src/Main.cpp
+++ src/Main.cpp
@@ -34,6 +34,7 @@ int main(int argc, char* argv[]) {
 
     mainWindow.show();
     mainWindow.setWindowTitle("QuickStatus2");
+    mainWindow.setWindowIcon(QIcon(":/icon.ico"));
 
     NTManager::StartClient();
 
